a80f01a342f48c8559f63d8d1cf4d5b454718259
matteo porta <matteo.sid@hotmail.it>
2022-09-18 02:38:48 +0200

    update

--- latexdockerdaemon.sh
+++ latexdockerdaemon.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-IMAGE=blang/latex:ubuntu
+IMAGE=0000matteo0000/latex:ubuntu
 exec docker run -d --rm --name latex_daemon -i --user="$(id -u):$(id -g)" --net=none -t -v $PWD:/data "$IMAGE" /bin/sh -c "sleep infinity"
